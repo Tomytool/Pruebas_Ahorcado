d58ad2753a2f3ca10449f9c60e6302a190a7fce9
Tomytool <56263086+Tomytool@users.noreply.github.com>
2021-10-28 13:26:18 -0300

    se realizan cambios Css y parte de la logica

---
+++ js/componentesLetras.js
@@ -0,0 +1,37 @@
+let abcedario = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'ñ',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+let contenedor = document.getElementById('letras');
+abcedario.forEach((letra, index) => {
+  let elemento = document.createElement('button');
+  elemento.classList.add('letra');
+  elemento.innerHTML = abcedario[index];
+
+  contenedor.appendChild(elemento);
+});
